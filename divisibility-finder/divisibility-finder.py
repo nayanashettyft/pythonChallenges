bafe1a2ee8fbd6211ecb18ff07ddf2ff2881d5ee
Nayana.Shetty <nayana.shetty@ft.com>
2018-09-27 21:53:49 +0100

    add classes for validation and division

--- divisibility-finder/divisibility-finder.py
+++ divisibility-finder/divisibility-finder.py
@@ -1,12 +1,22 @@
 #!/usr/local/bin/python3
 import argparse
 
+class ValidationProcessing:
+	def __init__(self, args):
+		for k,v in args.items():
+			setattr(self, k, v)
+
+
+class DivisionProcessing:
+    def __init__(self):
+        pass
+
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--number', required=True, type=int, help="--number NUMBER")
 	args = parser.parse_args()
-	print(vars(args))
+	vp = ValidationProcessing(vars(args))
 
 
 #  This is main
