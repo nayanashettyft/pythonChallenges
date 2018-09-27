6b1a86e5449c76aa6b0d5f08d082472eace53ced
Nayana.Shetty <nayana.shetty@ft.com>
2018-09-27 23:35:42 +0100

    Adding first test

---
+++ divisibility-finder/test/README.md
@@ -0,0 +1,15 @@
+# Validator tests
+These are tests for validation
+
+### Usage
+```
+pytest Validator.py
+```
+
+### Pre-requisites
+Make sure you have pytest installed
+
+Install pytest
+```
+pip install -U pytest
+```
---
+++ divisibility-finder/test/Validator.py
@@ -0,0 +1,8 @@
+import unittest
+from lib.Validator import ValidationProcessing
+
+class ValidationProcessingSpec(unittest.TestCase):
+
+    def test_ValidationProcessing_postive_number_returns_true(self):
+        vp = ValidationProcessing({'number': 2})
+        self.assertEqual(vp.is_positive(), True)
