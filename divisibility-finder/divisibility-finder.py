f7d0c920edd9f328d64bf5ea518a6cc900e6c947
Nayana.Shetty <nayana.shetty@ft.com>
2018-09-27 21:45:04 +0100

    taking input from users

--- divisibility-finder/divisibility-finder.py
+++ divisibility-finder/divisibility-finder.py
@@ -1,9 +1,14 @@
 #!/usr/local/bin/python3
+import argparse
+
 
 def main():
-	pass
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--number', required=True, type=int, help="--number NUMBER")
+	args = parser.parse_args()
+	print(vars(args))
 
-	
+
 #  This is main
 if __name__ == "__main__":
     main()
