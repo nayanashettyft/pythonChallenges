f728027fc415c340c12fb81d741a59fb148551a9
Nayana.Shetty <nayana.shetty@ft.com>
2018-09-27 22:00:26 +0100

    added number validation

--- divisibility-finder/divisibility-finder.py
+++ divisibility-finder/divisibility-finder.py
@@ -6,6 +6,11 @@ class ValidationProcessing:
 		for k,v in args.items():
 			setattr(self, k, v)
 
+	def is_positive(self):
+		if self.number > 0:
+			return True
+		else:
+			return False
 
 class DivisionProcessing:
     def __init__(self):
@@ -17,6 +22,10 @@ def main():
 	parser.add_argument('--number', required=True, type=int, help="--number NUMBER")
 	args = parser.parse_args()
 	vp = ValidationProcessing(vars(args))
+	if not vp.is_positive():
+		print("Number is not positive")
+		exit()
+	dp = DivisionProcessing()
 
 
 #  This is main
