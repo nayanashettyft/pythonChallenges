35ee46a520defd8ea56107352e5382d0167d8702
Nayana.Shetty <nayana.shetty@ft.com>
2018-09-28 13:08:26 +0100

    greater than test

--- test_examples/test_examples.py
+++ test_examples/test_examples.py
@@ -2,6 +2,11 @@
 def increment(x):
     return x + 1
 
+def isgreaterthan10(num):
+	if num > 10:
+		return True
+	else:
+		return False
 
 def test_increment_3_by_1_is_4():
     assert increment(3) == 4
@@ -21,3 +26,11 @@ def test_set_comparison():
     set1 = set("1234")
     assert set1 == {'2', '4', '1', '3'}
 
+def test_11_is_greater_than_10():
+	assert isgreaterthan10(11) == True
+
+def test_9_is_not_greater_than_10():
+	assert isgreaterthan10(9) == False
+
+def test_10_is_not_greater_than_10():
+	assert isgreaterthan10(10) == False
