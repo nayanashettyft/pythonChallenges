76799d7e706aa564d79822c6ad5d1f7fc8d00bcc
Nayana Shetty <nayanashetty@nayanas-mbp.home>
2018-10-17 23:19:42 +0100

    added tests for divide

--- calculator/test/test_calculator.py
+++ calculator/test/test_calculator.py
@@ -31,6 +31,25 @@ class TddInPythonAttempt(unittest.TestCase):
 		result = self.calc.validate_input(-2, -4)
 		self.assertEqual(1, result)
 
+	def test_calculator_divide_10_by_5_returns_2(self):
+		result = self.calc.divide(10,5)
+		self.assertEqual(2, result)
+
+	def test_calculator_divide_5_by_10_returns_half(self):
+		result = self.calc.divide(5,10)
+		self.assertEqual(0.5, result)
+
+	def test_calculator_divide_10_by_3_returns_3_33(self):
+		result = self.calc.divide(10,3)
+		self.assertEqual(3.33, result)
+
+	def test_calculator_divide_10_by_3_returns_3_33(self):
+		result = self.calc.divide(10,3)
+		self.assertEqual(3.33, result)
+
+	def test_calculator_divide_returns_error_message_if_both_args_not_numbers(self):
+		self.assertRaises(ValueError, self.calc.divide, 'two', 'three')
+
 	def test_calculator_add_method_returns_correct_result(self):
 		result = self.calc.add(2,2)
 		self.assertEqual(4, result)
